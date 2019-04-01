e21f7c708a72584f437d15a189409e451c23e0c8
passionbull <wnsl5684@gmail.com>
2019-04-02 04:40:39 +0900

    Updated voting page

--- src/Component/Voting.js
+++ src/Component/Voting.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import {
-  Text, Button,
+  Text, Button, FlatList,
   View, AsyncStorage
 } from 'react-native';
 import steemConnect from '../Steem/steemConnect';
 import steem from 'steem'
+import steemController from '../Steem/steemController'
+import VotingListItem from '../View/VotingListItem';
+import {Client} from 'dsteem'
+const client = new Client('https://api.steemit.com')
+
 
 export default class Voting extends Component {
     state = {
         modalVisible: false,
         sign_in: false,
-        _that: this
+        lp:'',la:'',
+        all_post:[]
     }
 
     onPressButton1 = () => {
@@ -20,40 +26,58 @@ export default class Voting extends Component {
         else
             this.revokeToken();
     }
-    onPressButton2 = () => {
-        var size = 5;
-        var query = {"tag": "jjm", "limit": size};
-        var last_permlink = ''
-        var last_author = ''
-        
-        steem.api.getDiscussionsByCreated(query, function(err, result) {
-            console.log(result);
-            last_permlink=result[size-1].permlink;
-            last_author= result[size-1].author;
-            // {"tag": "kr", "limit": 10, "start_permlink": window.permlink, "start_author": window.author}
-            query = {"tag": "jjm", "limit": 5, "start_permlink":last_permlink, "start_author":last_author};
-            steem.api.getDiscussionsByCreated(query, function(err, result) {
-                console.log(result);
-            });        
+    onPressButton2 = async () => {
+        var t = new steemController();
+        const size = 10;
+        t.getPosting(size,this, this.state.lp, this.state.la).then(function(posting){
+            console.log('post',posting.posts);
+            var lp = posting.last_permlink;
+            var la = posting.last_author;
+            var _all_post = posting.this.state.all_post.concat(posting.posts);
+            console.log('length',_all_post);
+            posting.this.setState({lp,la,all_post:_all_post});
         });
-
-
+    }
+    onPressButton3 = () => {
+        var _all_post = this.state.all_post;
+        var t = new steemController();
+        for (const post of _all_post) {
+            t.getVotingInfo(post);
+        }
     }
 
     render() {
         return (
         <View style={{flex: 1,}}>
-            <View style={{flex: 1, flexDirection: 'row', width:250, paddingLeft: 10, paddingTop: 20}}>
-                <View style={{flex: 1,paddingRight: 10}}>
+            <View style={{flex: 1, flexDirection: 'row', width:500, paddingLeft: 10, paddingTop: 20}}>
+                <View style={{flex: 1.2,paddingRight: 10}}>
                     <Button style={{}} title={this.state.sign_in === false? 'Sign in with SteemConnect':'Sign out'} onPress={this.onPressButton1}/>
                 </View>
-
-                <View style={{flex: 1,}}>
+                <View style={{flex: 1,paddingRight: 10}}>
                     <Button style={{}} title='Get Posts' onPress={this.onPressButton2}/>
                 </View>
-
+                <View style={{flex: 1,}}>
+                    <Button style={{}} title='Check Voted' onPress={this.onPressButton3}/>
+                </View>
             </View>
-            <Text style={{color: 'black', fontWeight: 'bold', fontSize: 20, paddingLeft: 10, paddingTop: 20, paddingBottom:3}}>Voting</Text>
+            <View style={{flex: 1, flexDirection: 'row', paddingLeft: 10, paddingTop: 20}}>
+                <Text style={{color: 'black', fontWeight: 'bold', fontSize: 20, paddingLeft: 10, paddingTop: 20, paddingBottom:3}}>Voting List:</Text>
+                <Text style={{color: 'black', fontWeight: 'bold', fontSize: 15, paddingLeft: 10, paddingTop: 20, paddingBottom:3}}>{this.state.all_post.length}</Text>
+            </View>
+            <View style={{flex: 1, paddingLeft: 20}}>
+                <View style={{flex: 1, flexDirection: 'row', paddingTop: 30, paddingLeft: 20}}>
+                    <Text style={{flex: 0.5, color: 'black', fontWeight: 'bold', fontSize: 20, padding: 3}}>Account</Text>
+                    <Text style={{flex: 1, color: 'black', fontWeight: 'bold', fontSize: 20, padding: 3}}>Title</Text>
+                    <Text style={{flex: 1, color: 'black', fontWeight: 'bold', fontSize: 20, padding: 3}}>Link</Text>
+                    <Text style={{flex: 1, color: 'black', fontWeight: 'bold', fontSize: 20, padding: 3}}>Date</Text>
+                    <Text style={{flex: 1,color: 'black', fontWeight: 'bold', fontSize: 20, padding: 3}}>Voted</Text>
+                </View>
+                <FlatList
+                    data= {this.state.all_post}
+                    renderItem={({item}) => <VotingListItem id={item.id} author ={item.author} permlink = {item.permlink} title = {item.title} created = {item.created} voted = {item.voted}/>}
+                    />
+            </View>
+
         </View>
         )
     }
@@ -128,4 +152,3 @@ export default class Voting extends Component {
     
 
 }
-// export default withRouter(Home);
